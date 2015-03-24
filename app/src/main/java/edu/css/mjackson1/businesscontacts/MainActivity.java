7106ed3411f1b478cf6b6e7cf04137dad38198a5
Michael Jackson <mj94@mj94.net>
2015-03-24 01:02:56 -0500

    Minor bug fixes and layout changes.

--- app/src/main/java/edu/css/mjackson1/businesscontacts/MainActivity.java
+++ app/src/main/java/edu/css/mjackson1/businesscontacts/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.layout_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
--- app/src/main/java/edu/css/mjackson1/businesscontacts/SecondActivity.java
+++ app/src/main/java/edu/css/mjackson1/businesscontacts/SecondActivity.java
@@ -3,13 +3,6 @@ package edu.css.mjackson1.businesscontacts;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.app.Activity;
-import android.content.Intent;
-import android.view.Menu;
-import android.view.View;
-import android.widget.TextView;
-
-
-import org.w3c.dom.Text;
 
 public class SecondActivity extends Activity {
 
@@ -19,7 +12,7 @@ public class SecondActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SetContentView(R.layout.layout_second);
+        setContentView(R.layout.layout_second);
 
         txtName = (TextView) findViewById(R.id.txtName);
         txtSummary = (TextView) findViewById(R.id.txtSummary);
@@ -35,19 +28,21 @@ public class SecondActivity extends Activity {
             txtName.setText("Best Buy");
             txtSummary.setText("Best Buy Co., Inc. is an American multinational consumer electronics corporation headquartered in Richfield, Minnesota, a Minneapolis suburb. It operates in the United States, Puerto Rico, Mexico, Canada, and China. The company was founded by Richard M. Schulze and Gary Smoliak in 1966 as an audio specialty store. In 1983, it was renamed and rebranded with more emphasis placed on consumer electronics.");
             txtLocation.setText("5105 Burning Tree Rd, Duluth, MN 55811");
-            txtHours.setText("Monday-Saturday: 10:00am to 9:00pm; Sunday: 10:00am to 8:00pm");
+            txtHours.setText("Monday-Saturday: 10:00am-9:00pm\nSunday: 10:00am-8:00pm");
         }
         if (businessNum.equals("2")) {
             txtName.setText("Target");
             txtSummary.setText("Target Corporation is an American retailing company, founded in 1902 and headquartered in Minneapolis, Minnesota. It is the second-largest discount retailer in the United States. The company is ranked 36th on the Fortune 500 as of 2013 and is a component of the Standard & Poor's 500 index.");
             txtLocation.setText("1902 Miller Trunk Hwy, Duluth, MN 55811");
-            txtHours.setText("Monday-Saturday: 8:00am-11:00pm; Sunday: 8:00am-10:00pm");
+            txtHours.setText("Monday-Saturday: 8:00am-11:00pm\nSunday: 8:00am-10:00pm");
         }
         if (businessNum.equals("3")) {
             txtName.setText("Texas Roadhouse");
-            txtSummary.setText("Target Corporation is an American retailing company, founded in 1902 and headquartered in Minneapolis, Minnesota. It is the second-largest discount retailer in the United States. The company is ranked 36th on the Fortune 500 as of 2013 and is a component of the Standard & Poor's 500 index.");
+            txtSummary.setText("Texas Roadhouse is an American chain restaurant that specializes in steaks and promotes a Western theme. Texas Roadhouse Corporation is headquartered in Louisville, Kentucky. The chain operates about 400 locations in 46 states. It is known for its free buckets of peanuts at each table.");
             txtLocation.setText("902 Mall Drive, Duluth, MN 55811");
-            txtHours.setText("Monday-Thursday: 4:00pm-10:00pm; Friday-Sunday: 11:00am-11:00pm");
+            txtHours.setText("Monday-Thursday: 4:00pm-10:00pm\nFriday-Sunday: 11:00am-11:00pm");
         }
     }
+
+
 }
